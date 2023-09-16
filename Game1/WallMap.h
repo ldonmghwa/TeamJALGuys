27381d981066303295a6b411d08171e77171a31c
Junhwan5245 <hdhd9127@gmail.com>
2023-09-16 16:45:26 +0900

    Revert "wallmap 클래스 수정"
    This reverts commit 4868fd0b128d5c271143f45419c6f22e6fd08fd1.

--- Game1/WallMap.h
+++ Game1/WallMap.h
@@ -5,31 +5,21 @@ public:
     static WallMap* Create(string name = "WallMap");
 
 public:
-    vector<GameObject*>   pillarUnderList;
-    vector<GameObject*>   pillarList;
-    vector<GameObject*>   glitingBoardList;
-    vector<GameObject*>   upDownBoardList;
-    vector<GameObject*>   leftRightBoardList;
+    list<GameObject*> moveObsColliderList;
+    list<GameObject*> obsColliderList2;
+    list<GameObject*> boardColliderList;
 private:
-    bool*                 isPillarUnderUp;
-    bool*                 isUpDownBoardUp;
-    bool*                 isLeftRightBoardGo;
-    bool                  isGlitingBoardChange = false;
-    float*                pillarUnderSpeed;
-    float*                upDownBoardSpeed;
-    float*                leftRightBoardSpeed;
-    float                 glitingBoardActiveTime;
-    float                 backUpGlitingBoardActiveTime;
-    vector<GameObject*>   glitingBoardList1;
-    vector<GameObject*>   glitingBoardList2;
-    vector<GameObject*>   upDownBoardList1;
-    vector<GameObject*>   upDownBoardList2;
+    bool* isUp;
+    bool isBoardChange = false;
+    int count = 0;
+    float* wallSpeed;
+    float boardActiveTime;
+    list<GameObject*> boardList1;
+    list<GameObject*> boardList2;
 private:
     WallMap();
     virtual ~WallMap();
 public:
-    void Update() override;
-    void LoadFile(string _file);
-    void LateUpdate();
+    void	Update() override;
+    void    LoadFile(string _file);
 };
-
