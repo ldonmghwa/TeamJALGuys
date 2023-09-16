c8eafa157d86ad273afa7d02c9db95afd49b5aea
Junhwan5245 <hdhd9127@gmail.com>
2023-09-16 21:32:53 +0900

    map 파일 수정

--- Game1/WallMap.cpp
+++ Game1/WallMap.cpp
@@ -11,8 +11,7 @@ WallMap* WallMap::Create(string name)
 
 WallMap::WallMap() : Map()
 {
-	glitingBoardActiveTime = 0.5f;
-	backUpGlitingBoardActiveTime = glitingBoardActiveTime;
+
 }
 
 WallMap::~WallMap()
@@ -20,49 +19,74 @@ WallMap::~WallMap()
 	delete[] pillarUnderSpeed;
 	delete[] upDownBoardSpeed;
 	delete[] leftRightBoardSpeed;
+	delete[] glitingBoardActiveTime;
+	delete[] backUpGlitingBoardActiveTime;
 	delete[] isPillarUnderUp;
 	delete[] isUpDownBoardUp;
 	delete[] isLeftRightBoardGo;
 }
 
+void WallMap::Init()
+{
+	ground0 = this->root->Find("Ground0");
+	ground1 = this->root->Find("Ground1");
+	ground2 = this->root->Find("Ground2");
+	ground3 = this->root->Find("Ground3");
+	goal = this->root->Find("Goal");
+}
+
 void WallMap::Update()
 {
-	for (int i = 0; i < pillarUnderList.size(); i++) {
-		if (isPillarUnderUp[i]) {
-			pillarUnderList[i]->MoveWorldPos(Vector3(0, 1, 0) * pillarUnderSpeed[i] * DELTA);
-			if (pillarUnderList[i]->GetLocalPos().y > 7.0f) isPillarUnderUp[i] = false;
-		}
-		else {
-			pillarUnderList[i]->MoveWorldPos(Vector3(0, -1, 0) * pillarUnderSpeed[i] * DELTA);
-			if (pillarUnderList[i]->GetLocalPos().y < -7.0f) isPillarUnderUp[i] = true;
-		}
-	}
-	for (int i = 0; i < upDownBoardList.size(); i++) {
-		if (isUpDownBoardUp[i]) {
-			upDownBoardList[i]->MoveWorldPos(Vector3(0, 1, 0) * upDownBoardSpeed[i] * DELTA);
-			if (upDownBoardList[i]->GetLocalPos().y > 5.0f) isUpDownBoardUp[i] = false;
-		}
-		else {
-			upDownBoardList[i]->MoveWorldPos(Vector3(0, -1, 0) * upDownBoardSpeed[i] * DELTA);
-			if (upDownBoardList[i]->GetLocalPos().y < -5.0f) isUpDownBoardUp[i] = true;
+	{
+		for (int i = 0; i < pillarUnderList.size(); i++) {
+			if (isPillarUnderUp[i]) {
+				pillarUnderList[i]->MoveWorldPos(Vector3(0, 1, 0) * pillarUnderSpeed[i] * DELTA);
+				if (pillarUnderList[i]->GetLocalPos().y > 9.0f) isPillarUnderUp[i] = false;
+			}
+			else {
+				pillarUnderList[i]->MoveWorldPos(Vector3(0, -1, 0) * pillarUnderSpeed[i] * DELTA);
+				if (pillarUnderList[i]->GetLocalPos().y < -7.0f) isPillarUnderUp[i] = true;
+			}
 		}
 	}
-	for (int i = 0; i < leftRightBoardList.size(); i++) {
-		if (isLeftRightBoardGo[i]) {
-			leftRightBoardList[i]->MoveWorldPos(Vector3(1, 0, 0) * leftRightBoardSpeed[i] * DELTA);
-			if (leftRightBoardList[i]->GetLocalPos().x > 19.0f) isLeftRightBoardGo[i] = false;
+	{
+		for (int i = 0; i < upDownBoardList.size(); i++) {
+			if (isUpDownBoardUp[i]) {
+				upDownBoardList[i]->MoveWorldPos(Vector3(0, 1, 0) * upDownBoardSpeed[i] * DELTA);
+				if (upDownBoardList[i]->GetLocalPos().y > 5.0f) isUpDownBoardUp[i] = false;
+			}
+			else {
+				upDownBoardList[i]->MoveWorldPos(Vector3(0, -1, 0) * upDownBoardSpeed[i] * DELTA);
+				if (upDownBoardList[i]->GetLocalPos().y < -5.0f) isUpDownBoardUp[i] = true;
+			}
 		}
-		else {
-			leftRightBoardList[i]->MoveWorldPos(Vector3(-1, 0, 0) * leftRightBoardSpeed[i] * DELTA);
-			if (leftRightBoardList[i]->GetLocalPos().x < -19.0f) isLeftRightBoardGo[i] = true;
+		for (int i = 0; i < leftRightBoardList.size(); i++) {
+			if (isLeftRightBoardGo[i]) {
+				leftRightBoardList[i]->MoveWorldPos(Vector3(1, 0, 0) * leftRightBoardSpeed[i] * DELTA);
+				if (leftRightBoardList[i]->GetLocalPos().x > 19.0f) isLeftRightBoardGo[i] = false;
+			}
+			else {
+				leftRightBoardList[i]->MoveWorldPos(Vector3(-1, 0, 0) * leftRightBoardSpeed[i] * DELTA);
+				if (leftRightBoardList[i]->GetLocalPos().x < -19.0f) isLeftRightBoardGo[i] = true;
+			}
 		}
 	}
-	glitingBoardActiveTime -= DELTA;
+	{
+		for (int i = 0; i < glitingBoardList.size(); i++) {
+			glitingBoardActiveTime[i] -= DELTA;
+			if (glitingBoardActiveTime[i] < 0) {
+				glitingBoardList[i]->visible = not glitingBoardList[i]->visible;
+				glitingBoardActiveTime[i] = backUpGlitingBoardActiveTime[i];
+			}
+		}
+	}
+
+	/*glitingBoardActiveTime -= DELTA;
 	if (glitingBoardActiveTime < 0) {
 		for (int i = 0; i < glitingBoardList.size(); i++) glitingBoardList[i]->visible = not glitingBoardList[i]->visible;
 		glitingBoardActiveTime = backUpGlitingBoardActiveTime;
 		isGlitingBoardChange = not isGlitingBoardChange;
-	}
+	}*/
 
 	Map::Update();
 }
@@ -74,49 +98,41 @@ void WallMap::LoadFile(string _file)
 		int pillarUnderCount = 0;
 		while (this->root->Find("Pillar1_under" + to_string(pillarUnderCount))) {
 			pillarUnderList.push_back(this->root->Find("Pillar1_under" + to_string(pillarUnderCount)));
-			pillarUnderList.back()->scale.y = 7;
 			pillarUnderCount++;
 		}
 		pillarUnderCount = 0;
 		while (this->root->Find("Pillar2_under" + to_string(pillarUnderCount))) {
 			pillarUnderList.push_back(this->root->Find("Pillar2_under" + to_string(pillarUnderCount)));
-			pillarUnderList.back()->scale.y = 7;
 			pillarUnderCount++;
 		}
 		pillarUnderCount = 0;
 		while (this->root->Find("Pillar3_under" + to_string(pillarUnderCount))) {
 			pillarUnderList.push_back(this->root->Find("Pillar3_under" + to_string(pillarUnderCount)));
-			pillarUnderList.back()->scale.y = 7;
 			pillarUnderCount++;
 		}
 		pillarUnderCount = 0;
 		while (this->root->Find("Pillar4_under" + to_string(pillarUnderCount))) {
 			pillarUnderList.push_back(this->root->Find("Pillar4_under" + to_string(pillarUnderCount)));
-			pillarUnderList.back()->scale.y = 7;
 			pillarUnderCount++;
 		}
 		int pillarCount = 0;
 		while (this->root->Find("Pillar1_" + to_string(pillarCount))) {
 			pillarList.push_back(this->root->Find("Pillar1_" + to_string(pillarCount)));
-			pillarList.back()->scale.y = 10;
 			pillarCount++;
 		}
 		pillarCount = 0;
 		while (this->root->Find("Pillar2_" + to_string(pillarCount))) {
 			pillarList.push_back(this->root->Find("Pillar2_" + to_string(pillarCount)));
-			pillarList.back()->scale.y = 10;
 			pillarCount++;
 		}
 		pillarCount = 0;
 		while (this->root->Find("Pillar3_" + to_string(pillarCount))) {
 			pillarList.push_back(this->root->Find("Pillar3_" + to_string(pillarCount)));
-			pillarList.back()->scale.y = 10;
 			pillarCount++;
 		}
 		pillarCount = 0;
 		while (this->root->Find("Pillar4_" + to_string(pillarCount))) {
 			pillarList.push_back(this->root->Find("Pillar4_" + to_string(pillarCount)));
-			pillarList.back()->scale.y = 10;
 			pillarCount++;
 		}
 	}
@@ -151,38 +167,77 @@ void WallMap::LoadFile(string _file)
 			boardCount++;
 		}
 	}
-
-	int upDownBoardCount = 0;
-	while (this->root->Find("UpDownBoard_" + to_string(upDownBoardCount))) {
-		upDownBoardList.push_back(this->root->Find("UpDownBoard_" + to_string(upDownBoardCount)));
-		upDownBoardCount++;
+	{
+		int upDownBoardCount = 0;
+		while (this->root->Find("UpDownBoard_" + to_string(upDownBoardCount))) {
+			upDownBoardList.push_back(this->root->Find("UpDownBoard_" + to_string(upDownBoardCount)));
+			upDownBoardCount++;
+		}
+		int leftRightBoardCount = 0;
+		while (this->root->Find("LeftRightBoard_" + to_string(leftRightBoardCount))) {
+			leftRightBoardList.push_back(this->root->Find("LeftRightBoard_" + to_string(leftRightBoardCount)));
+			leftRightBoardCount++;
+		}
 	}
-	int leftRightBoardCount = 0;
-	while (this->root->Find("LeftRightBoard_" + to_string(leftRightBoardCount))) {
-		leftRightBoardList.push_back(this->root->Find("LeftRightBoard_" + to_string(leftRightBoardCount)));
-		leftRightBoardCount++;
+	{
+		pillarUnderSpeed = new float[pillarUnderList.size()];
+		upDownBoardSpeed = new float[upDownBoardList.size()];
+		leftRightBoardSpeed = new float[leftRightBoardList.size()];
+		glitingBoardActiveTime = new float[glitingBoardList.size()];
+		backUpGlitingBoardActiveTime = new float[glitingBoardList.size()];
+		isPillarUnderUp = new bool[pillarUnderList.size()];
+		isUpDownBoardUp = new bool[upDownBoardList.size()];
+		isLeftRightBoardGo = new bool[leftRightBoardList.size()];
+		landingYPos = glitingBoardList.back()->scale.y;
+		//playerScaleY = player->body->scale.y * 2.0f;
 	}
-	pillarUnderSpeed = new float[pillarUnderList.size()];
-	upDownBoardSpeed = new float[upDownBoardList.size()];
-	leftRightBoardSpeed = new float[leftRightBoardList.size()];
-	isPillarUnderUp = new bool[pillarUnderList.size()];
-	isUpDownBoardUp = new bool[upDownBoardList.size()];
-	isLeftRightBoardGo = new bool[leftRightBoardList.size()];
-	for (int i = 0; i < pillarUnderList.size(); i++) {
-		pillarUnderSpeed[i] = RANDOM->Float(20, 50);
-		isPillarUnderUp[i] = false;
-	}
-	for (int i = 0; i < upDownBoardList.size(); i++) {
-		upDownBoardSpeed[i] = RANDOM->Float(10, 20);
-		isUpDownBoardUp[i] = RANDOM->Int(0, 1);
-	}
-	for (int i = 0; i < leftRightBoardList.size(); i++) {
-		leftRightBoardSpeed[i] = RANDOM->Float(70, 100);
-		isLeftRightBoardGo[i] = RANDOM->Int(0, 1);
+	{
+		for (int i = 0; i < pillarUnderList.size(); i++) {
+			pillarUnderSpeed[i] = RANDOM->Float(10, 30);
+			isPillarUnderUp[i] = false;
+		}
+		for (int i = 0; i < upDownBoardList.size(); i++) {
+			upDownBoardSpeed[i] = RANDOM->Float(20, 30);
+			isUpDownBoardUp[i] = RANDOM->Int(0, 1);
+		}
+		for (int i = 0; i < leftRightBoardList.size(); i++) {
+			leftRightBoardSpeed[i] = RANDOM->Float(50, 60);
+			isLeftRightBoardGo[i] = RANDOM->Int(0, 1);
+		}
+		for (int i = 0; i < glitingBoardList.size(); i++) {
+			glitingBoardActiveTime[i] = RANDOM->Float(2.0f, 6.5f);
+			//glitingBoardActiveTime[i] = 100.0f;
+			backUpGlitingBoardActiveTime[i] = glitingBoardActiveTime[i];
+		}
 	}
 }
-
 
 void WallMap::LateUpdate()
 {
+	/*ImGui::Text("pl pos: %f, %f, %f", player->body->GetWorldPos().x, player->body->GetWorldPos().y, player->body->GetWorldPos().z);
+	if (player->body->Intersect(ground0)) player->isLand = true;
+	else if (player->body->Intersect(ground1)) player->isLand = true;
+	else if (player->body->Intersect(ground2)) player->isLand = true;
+	else if (player->body->Intersect(ground3)) player->isLand = true;
+	else if (player->body->Intersect(goal)) player->isLand = true;
+	else player->isLand = false;
+
+	for (int i = 0; i < pillarUnderList.size(); i++) {
+		if (pillarUnderList[i]->Intersect(player->body)) {
+			player->body->MoveWorldPos(Vector3(0.0f, 0.0f, -1.0f) * 0.1f);
+		}
+	}
+	for (int i = 0; i < pillarList.size(); i++) {
+		if (pillarList[i]->Intersect(player->body)) {
+			player->body->MoveWorldPos(Vector3(0.0f, 0.0f, -1.0f) * 0.1f);
+		}
+	}
+	for (int i = 0; i < glitingBoardList.size(); i++) {
+		if (glitingBoardList[i]->Intersect(player->body)) {
+			if (glitingBoardList[i]->visible) player->isLand = true;
+			else player->isLand = false;
+		}
+	}*/
+
 }
+
--- Game1/WallMap.h
+++ Game1/WallMap.h
@@ -11,23 +11,32 @@ public:
     vector<GameObject*>   upDownBoardList;
     vector<GameObject*>   leftRightBoardList;
 private:
-    bool* isPillarUnderUp;
-    bool* isUpDownBoardUp;
-    bool* isLeftRightBoardGo;
+    bool*                 isPillarUnderUp;
+    bool*                 isUpDownBoardUp;
+    bool*                 isLeftRightBoardGo;
     bool                  isGlitingBoardChange = false;
-    float* pillarUnderSpeed;
-    float* upDownBoardSpeed;
-    float* leftRightBoardSpeed;
-    float                 glitingBoardActiveTime;
-    float                 backUpGlitingBoardActiveTime;
+    float*                pillarUnderSpeed;
+    float*                upDownBoardSpeed;
+    float*                leftRightBoardSpeed;
+    float*                glitingBoardActiveTime;
+    float*                backUpGlitingBoardActiveTime;
+    float                 landingYPos;
+    float                 playerScaleY;
     vector<GameObject*>   glitingBoardList1;
     vector<GameObject*>   glitingBoardList2;
     vector<GameObject*>   upDownBoardList1;
     vector<GameObject*>   upDownBoardList2;
+
+    GameObject*           ground0;
+    GameObject*           ground1;
+    GameObject*           ground2;
+    GameObject*           ground3;
+    GameObject*           goal;
 private:
     WallMap();
     virtual ~WallMap();
 public:
+    void Init();
     void Update() override;
     void LoadFile(string _file);
     void LateUpdate();
