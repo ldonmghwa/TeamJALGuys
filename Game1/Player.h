d4ec8e01917589e291c94c37f717a3e73aafacdd
biBeam <lovewonvv@gmail.com>
2023-09-16 21:21:06 +0900

    플레이어 텍스쳐/움직임 수정

--- Game1/Player.h
+++ Game1/Player.h
@@ -11,12 +11,17 @@ private:
 	float moveSpeed = 0;
 	float jumpmoveSpeed = 0;
 	float gravity = 0;
+	float gravityPower = 0;
 	float diveTime = 0.3f;
 	float diveCool = 0;
-	const float jumpPower = 15.0f;
+	float jumpPower = 0;
 	const float divePower = 30.0f;
 
 	bool isJump = 0;
+	bool motionR = 0;
+	bool motionJ = 0;
+	bool motionD = 0;
+	float motionDir = 0;
 public:
 	Actor* body;
 
